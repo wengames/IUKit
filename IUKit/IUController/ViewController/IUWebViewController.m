41dbbf49cbbf128723c25b781902ffa1bcba5947
刘海文 <liuhaiwen@wondersgroup.com>
2017-02-15 17:58:57 +0800

    IUWebViewController

---
+++ IUKit/IUController/ViewController/IUWebViewController.m
@@ -0,0 +1,37 @@
+//
+//  IUWebViewController.m
+//  IUKitDemo
+//
+//  Created by admin on 2017/2/15.
+//  Copyright © 2017年 刘海文. All rights reserved.
+//
+
+#import "IUWebViewController.h"
+
+@interface IUWebViewController ()
+
+@end
+
+@implementation IUWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
