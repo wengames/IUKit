8ad85742a5bcdc9d44ef07c88ec6bd5f2312288e
刘海文 <liuhaiwen@wondersgroup.com>
2017-02-17 11:22:40 +0800

    update IURefresh

---
+++ IUKit/IUUtil/Categories/UIScrollView+IURefresh.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+IURefresh.h
+//  IUKitDemo
+//
+//  Created by admin on 2017/2/17.
+//  Copyright © 2017年 刘海文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (IURefresh)
+
+@end
---
+++ IUKit/IUUtil/Categories/UIScrollView+IURefresh.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+IURefresh.m
+//  IUKitDemo
+//
+//  Created by admin on 2017/2/17.
+//  Copyright © 2017年 刘海文. All rights reserved.
+//
+
+#import "UIScrollView+IURefresh.h"
+
+@implementation UIScrollView (IURefresh)
+
+@end
